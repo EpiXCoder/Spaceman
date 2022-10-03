bd25dd10a5bffe335e448f91ffc27198ac8249e6
Vithusha Ravirajan <vithusharavirajan@Vithushas-MacBook-Pro.local>
2022-10-02 22:56:53 -0400

    Made great progress. The game is functional. Need to make final touch-ups

--- code.py
+++ code.py
@@ -4,25 +4,21 @@ import random
 
 from colorama import Fore 
 
-# def load_word():
-#     f = open('words.txt', 'r')
-#     words_list = f.readlines()
-#     f.close()
+def load_word():
+    f = open('words.txt', 'r')
+    words_list = f.readlines()
+    f.close()
     
-#     words_list = words_list[0].split(' ') 
-#     secret_word = random.choice(words_list)
-#     # print (secret_word)
-#     return secret_word
+    words_list = words_list[0].split(' ') 
+    selected_list = []
+    for word in words_list:
+        if len(word) <= 7:
+            selected_list.append(word)
+    selected_word = random.choice(selected_list)
+    secret_word = list(selected_word)
+    # print (secret_word)
+    return secret_word
 
-# secret_word = list(load_word())
-secret_word = ['a', 'p', 'p', 'l', 'e']
-print(secret_word)
-secret_word_joint = ''.join(secret_word)
-print (secret_word_joint)
-hidden_word = list(len(secret_word) * '_')
-print(hidden_word)
-letters_guessed = ['a', 'n', 'e', 't' ]
-guess = 'l'
 
 def is_word_guessed(secret_word, letters_guessed):
 
@@ -90,12 +86,19 @@ def is_guess_in_word(guess, secret_word):
 
 
     #TODO: check if the letter guess is in the secret word
-
+def guessed (guess):
+    letters_guessed.append(guess)
 
 
 # print (is_guess_in_word(guess, secret_word))
 
 # welcomeMessage = 'WELCOME to SPACE MAN'
+
+def user_input(prompt):
+    user_input = input(prompt)
+    return user_input
+
+
 
 
 def spaceman(secret_word):
@@ -115,24 +118,88 @@ def spaceman(secret_word):
         print(colors[random.choice(list(colors.keys()))] + character, end='')
     for character in starBorder:
         print(colors[random.choice(list(colors.keys()))] + character, end='')
+   
+    print(colors['CYAN'] + "RULES OF THE GAME:\n- You will be guessing the secret word one letter at a time\n- You will have 7 attempts to guess the word correctly.\n- Ready? Let's Go!")
+    print()
+    print(colors['GREEN'] + "Secret Word has " + str(len(secret_word)) + " letters.")
+    print(colors['GREEN'] + "The Secret Word: " + (' '.join(hidden_word)))
+    print()
+    guess_left = 7
+    run = True
+    while run or guess_left > 0:
+        if guess_left == 0 and run ==True:
+            print ()
+            print (colors['RED'] + "OH NO! You've run out of guesses.")
+            print ()
+            break
 
+        guess = user_input(colors['BLUE'] + "Enter guess one letter at a time: ")
+        while len(guess) != 1 or guess.isalpha() == False or guess.strip() == '':
+            print(' ')
+            print (colors['RED'] + "Invalid Entry.")
+            print(' ')
+            guess = user_input(colors['BLUE'] + "Enter guess one letter at a time: ")
+
+        while guess in hidden_word or guess in letters_guessed:
+            print ()
+            print (colors['RED'] + "You've already guessed '" + str(guess) + "'!")
+            print ()
+            guess = user_input(colors['BLUE'] + "Enter guess one letter at a time: ")
+        
+        guess_left -= 1
+        print ()
+        if is_guess_in_word(guess, secret_word):
+            guessed (guess)
+            get_guessed_word(secret_word, letters_guessed, hidden_word)
+            print (colors['GREEN'] + "Great!! '" + str(guess) + "' is in the secret word.")
+        else: 
+            guessed (guess)
+            print (colors['RED'] + "Sorry! '" + str(guess) + "' is not in the secret word.")
+        
+        print ()
+        print(colors['GREEN'] + "The Secret Word: " + (' '.join(hidden_word)))
+        print(colors['YELLOW'] + "Letters guessed so far: " + (','.join(letters_guessed)) )
+        print(colors['YELLOW'] + "Guesses left: " + str(guess_left) )
+        if is_word_guessed(secret_word, letters_guessed):
+            run = False
+        if run == False:
+            break
+
+
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+# START GAME!
+
+secret_word = load_word()
+# secret_word = ['a', 'p', 'p', 'l', 'e']
+secret_word_joint = ''.join(secret_word)
+hidden_word = list(len(secret_word) * '_')
+letters_guessed = [ ]
+    
 spaceman(secret_word)
 
-    #TODO: show the player information about the game according to the project spec
+colors = dict(Fore.__dict__.items())
+starBorder = '*****************************\n'
+for character in starBorder:
+    print(colors[random.choice(list(colors.keys()))] + character, end='')
 
-    #TODO: Ask the player to guess one letter per round and check that it is only one letter
+if is_word_guessed (secret_word, letters_guessed):
+    print(colors['GREEN'] + "CONGRATULATIONS! You win! You've guessed the word correctly!")
+    print(colors['GREEN'] + "The secret word was: " + (''.join(secret_word)))
+    
+else:
+    print (colors['RED'] + "Sorry! You lose. You did not guess the word correctly!")
+    print (colors['GREEN'] + "The secret word was: " + (''.join(secret_word)))
 
-    #TODO: Check if the guessed letter is in the secret or not and give the player feedback
+for character in starBorder:
+    print(colors[random.choice(list(colors.keys()))] + character, end='')
 
-    #TODO: show the guessed word so far
-
-    #TODO: check if the game has been won or lost
-
+  
 
 
 
 
 
-#These function calls that will start the game
-# secret_word = load_word()
-# spaceman(secret_word)
+
